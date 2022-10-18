25ecad4072bc3b00ca408fadda94c7ee02e0c67a
Melvin van den Brink <melvinvdb@hotmail.com>
2022-10-18 14:34:38 +0200

    Add credit amount and dateconvertor

--- src/transactions/Transactions.jsx
+++ src/transactions/Transactions.jsx
@@ -15,9 +15,6 @@ function Transactions() {
               setIsLoaded(true);
               setTransactions(result.transactions);
             },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
             (error) => {
               setIsLoaded(true);
               setError(error);
@@ -25,7 +22,9 @@ function Transactions() {
           )
       }, [])
 
-
+      const dateConvertor = (dateAsString) => {
+        return new Date(dateAsString).toLocaleString('nl-NL', {dateStyle: 'medium'})
+      };
 
       if (error) {
         return <div>Error: {error.message}</div>;
@@ -36,20 +35,19 @@ function Transactions() {
             <div>
                 <h1>Transactie overzicht</h1>
                 <div className="container">
-                    <h2>Deze maand</h2>
+                    <h2>Alle transacties</h2>
                     <div>
                         {transactions.map(transaction => (
                             <div className="transaction" key={transaction.id}>
                                 <div className="transaction-amount">
-                                    <span className='amount-debit'>
+                                    <span className={transaction.amount > 0 ? "amount-debit" : "amount-credit"}>
                                         &euro; {transaction.amount}
                                     </span>
                                 </div>
                                 <div className="transaction-description">{transaction.description}</div>
-                                <div className="transaction-date">{transaction.dateTime}</div>
+                                <div className="transaction-date">{dateConvertor(transaction.dateTime)}</div>
                             </div>
                         ))}
-    
                     </div>
                 </div>
             </div>
