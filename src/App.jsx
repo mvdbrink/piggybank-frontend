0265b9a3ca9317a5d0ad630000c288a41d237bb0
Melvin van den Brink <melvinvdb@hotmail.com>
2022-11-09 22:28:45 +0100

    login

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import Overview from './pages/overview/Overview';
 import { Route, Routes, NavLink } from "react-router-dom";
 import Transfer from './pages/transfer/Transfer';
 import TransactionOverview from './pages/transactions-overview/Transaction-Overview';
+import Protected from './components/protected/Protected';
 import Settings from './pages/settings/Settings';
+import { useState } from 'react';
+import Login from './pages/login/Login';
+import { useEffect } from 'react';
+
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+
+  useEffect(() => {
+    const localData = localStorage.getItem('userData');
+    if (localData) {
+      setIsLoggedIn(true);
+    }
+  }, [isLoggedIn]);
+  
+
+
+
   return (
     <div className="app">
       <div className="sidenav">
@@ -28,10 +45,11 @@ function App() {
       </div>
       <main>
         <Routes>
-          <Route path="/" element={<Overview />}></Route>
-          <Route path="/transactions" element={<TransactionOverview />}></Route>
-          <Route path="/transfer" element={<Transfer />}></Route>
-          <Route path="/settings" element={<Settings />}></Route>
+          <Route path="/transactions" element={<Protected isLoggedIn={isLoggedIn}><TransactionOverview /></Protected>} />
+          <Route path="/" element={<Protected isLoggedIn={isLoggedIn}><Overview /></Protected>}></Route>
+          <Route path="/transfer" element={<Protected isLoggedIn={isLoggedIn}><Transfer /></Protected>}></Route>
+          <Route path="/settings" element={<Protected isLoggedIn={isLoggedIn}><Settings /></Protected>}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="*" element={<div className=""><h1>Pagina niet gevonden.</h1><img src="/images/not-found.gif" alt="not-found" /></div>}></Route>
         </Routes>
       </main>
